830c29396812630d7a34a9452fef44d0ff27c2be
YukunJ <yukunj@andrew.cmu.edu>
2022-11-19 19:42:19 -0500

    Route default to index.html

--- src/server.c
+++ src/server.c
@@ -296,10 +296,17 @@ int main(int argc, char *argv[]) {
                         test_error_code_t result_code = parse_http_request(poll_array->buffers[i], poll_array->sizes[i],
                                                                            &request, &read_amount);
                         while (poll_array->sizes[i] > 0) {
+                            char *response;
+
+                            size_t response_len;
+                            serialize_http_response(&response, &response_len, BAD_REQUEST_SHORT, NULL, NULL,
+                                                    NULL, 0, NULL, true);
+                            robust_write(ready_fd, response, response_len);
+                            free(response);
                             /* Debug: send 400 anyway */
-                            const char *bad_request_char = "HTTP/1.1 400 Bad Request\r\nConnection: Close\r\n\r\n";
-                            robust_write(ready_fd, bad_request_char, strlen(bad_request_char));
-                            remove_from_poll_array(i, poll_array);
+                            // const char *bad_request_char = "HTTP/1.1 400 Bad Request\r\nConnection: Close\r\n\r\n";
+                            // robust_write(ready_fd, bad_request_char, strlen(bad_request_char));
+                            // remove_from_poll_array(i, poll_array);
                             break;
                             if (result_code == TEST_ERROR_PARSE_PARTIAL) {
                                 const char *bad_request_char = "HTTP/1.1 400 Bad Request\r\nConnection: Close\r\n\r\n";
